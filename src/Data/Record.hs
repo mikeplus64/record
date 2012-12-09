51f38e6a1ff4e16aaf4f1809dbf2fc6c42833050
Mike Ledger <eleventynine@gmail.com>
2012-12-09 14:42:19 +1100

    refactor QQs; readd Set, redo Modify

--- src/Data/Record.hs
+++ src/Data/Record.hs
@@ -1,20 +1,26 @@
--- | Type safe labelled records implemented using GADTs, type literals, and other fun stuff.
+-- | Simple, type safe labelled records implemented using GADTs, type literals, and other fun stuff.
 -- Functions that use TH are for convenience only -- they are only there to save you from typing a few extra characters
 --
 -- Extensions you need to use this module are: TypeOperators, DataKinds
 --
 
-{-# LANGUAGE UndecidableInstances, OverlappingInstances, TypeFamilies, RecordWildCards, DataKinds, TypeOperators, FunctionalDependencies, GADTs, KindSignatures, FlexibleInstances, MultiParamTypeClasses, TemplateHaskell, PolyKinds, EmptyDataDecls #-}
+{-# LANGUAGE UndecidableInstances, OverlappingInstances, TypeFamilies, RecordWildCards, DataKinds, TypeOperators, FunctionalDependencies, GADTs, KindSignatures, FlexibleInstances, MultiParamTypeClasses, TemplateHaskell, PolyKinds, EmptyDataDecls, ScopedTypeVariables #-}
 module Data.Record 
     ( n
     , N
-    , key
+
+    , update
+    , access
+    , modify
+    , write
+
     , set
     , get
+    , alt
+    , modi
+
     , type (++)
     , Record(..)
-    , ($=)
-    , access
     , (:+)
     , (::=)
     , (+++) ) where
@@ -29,38 +35,28 @@ data N :: Symbol -> *
 n :: N k
 n = undefined
 
--- | More convenience; @ [key|k|] == n :: N k @
-key :: QuasiQuoter
-key = QuasiQuoter {..}
-  where 
-    quoteExp  s = [|undefined :: N $(clean s)|]
-    quotePat  s = [p|_|] `sigP`[t|N $(clean s)|]
-    quoteType s = [t|N $(clean s)|]
-    quoteDec    = undefined
-
 clean :: String -> TypeQ
 clean = litT . strTyLit . unwords . words
 
--- | Convenience function making it nicer to update a field of a record.
--- Instead of @ (n :: N k) $= a @ or @ [key|k|] $= a @, you can use @ [set|k|] a @
+-- | See 'write'.
 set :: QuasiQuoter
-set = QuasiQuoter{..}
-  where
-    quoteExp s = [|($=) (undefined :: N $(clean s))|]
-    quotePat   = undefined
-    quoteType  = undefined
-    quoteDec   = undefined
+set = QuasiQuoter{ quoteExp = \s -> [|write (undefined :: N $(clean s))|] }
 
--- | Convenience function making it nicer to get a field of a record.
--- Instead of @ r ! (n :: N k) @, or @ r ! [key|k|] @, you can use @ [get|k|] r @
+-- | See 'update'
+upd :: QuasiQuoter
+upd = QuasiQuoter{ quoteExp = \s -> [|update (undefined :: N $(clean s))|] }
+
+-- | See 'alter'
+alt :: QuasiQuoter
+alt = QuasiQuoter{ quoteExp = \s -> [|alter (undefined :: N $(clean s))|] }
+
+-- | See 'modify'
+modi :: QuasiQuoter
+modi = QuasiQuoter{ quoteExp = \s -> [|modify (undefined :: N $(clean s))|] }
+
+-- | See 'access'
 get :: QuasiQuoter
-get = QuasiQuoter{..} 
-  where
-    quoteExp s = [|flip access (undefined :: N $(clean s))|]
-    quotePat   = undefined
-    quoteType  = undefined
-    quoteDec   = undefined
-
+get = QuasiQuoter{ quoteExp = \s -> [|flip access (undefined :: N $(clean s))|] }
 
 data Record :: [(Symbol, *)] -> * where 
     End   :: Record '[]
@@ -80,7 +76,7 @@ infixr 3 :::
 instance Show (Record '[]) where
     show _ = "End"
 
-instance (Show a, Show (Record xs)) => Show (Record ( '(k, a) ': xs)) where
+instance (Show a, Show (Record xs)) => Show (Record ( '(k,a) ': xs)) where
     show (x ::: xs) = show x ++ " ::: " ++ show xs
 
 instance Eq (Record '[]) where
@@ -92,51 +88,50 @@ instance (Eq a, Eq (Record xs)) => Eq (Record ('(k, a) ': xs)) where
 instance Ord (Record '[]) where
     compare _ _ = EQ
 
-instance (Ord a, Ord (Record xs)) => Ord (Record ('(k, a) ': xs)) where
+instance (Ord a, Ord (Record xs)) => Ord (Record ('(k,a) ': xs)) where
     compare (x ::: xs) (y ::: ys) = compare (compare x y) (compare xs ys)
 
 
 
 
-class Get (r :: [(Symbol, *)]) (k :: Symbol) a | r k -> a where
+class Get (r :: [(Symbol,*)]) (k :: Symbol) a | r k -> a where
     -- | Get a field of a record.
     access :: Record r -> N k -> a
 
--- Success on the first element of a single element record
-instance Get '[ '(k, a) ] k a where
+instance Get ('(k,a) ': xs) k a where
     access (a ::: _) _ = a
 
--- Success on the first element of a record with 1 or more elements
-instance Get ('(k, a) ': xs) k a where
-    access (a ::: _) _ = a
-
--- Fail the first element, try the rest
-instance Get xs k a => Get ('(k1, a1) ': xs) k a where
+instance Get xs k a => Get ('(k1,a1) ': xs) k a where
     access (_ ::: a) f = access a f
 
-
-
-class Update (r :: [(Symbol, *)]) (k :: Symbol) a | r k -> a where
-    -- | Update a field of a record.
-    ($=) :: N k -> a -> Record r -> Record r
-
--- Succeed on the first try
-instance Update ( '(k, a) ': xs) k a where
-    (_ $= x) (_ ::: b) = x ::: b
-
--- Fail and try the rest of the record
-instance Update xs k a => Update ( '(k1,a1) ': xs) k a where
-    (f $= x) (a ::: b) = a ::: (f $= x) b
-
--- Modify a record's field. Changing its type is perfectly O.K.
-class Modify (r0 :: [(Symbol, *)]) (k :: Symbol) a b (r1 :: [(Symbol, *)]) | r0 k a b -> r1 where
+class Modify (r0 :: [(Symbol,*)]) (k :: Symbol) a b (r1 :: [(Symbol,*)]) | r0 k a b -> r1 where
+    -- | Modify a record's field, with the ability to change its type.
     modify :: N k -> (a -> b) -> Record r0 -> Record r1
 
-instance Modify ( '(k, a) ': xs) k a b ( '(k, b) ': xs) where
+instance Modify ('(k, a) ': xs) k a b ('(k, b) ': xs) where
     modify _ f (a ::: xs) = f a ::: xs
 
-instance Modify xs k a b ys => Modify ( '(k1, a1) ': xs) k a b ( '(k1, a1) ': ys ) where
+instance Modify xs k a b ys => Modify ('(k1,a1) ': xs) k a b ('(k1,a1) ': ys) where
     modify l f (a ::: xs) = a ::: modify l f xs
+
+
+class Set (r0 :: [(Symbol,*)]) k a b (r1 :: [(Symbol,*)]) | r0 k -> a where
+    -- | Write a record's field, with the ability to change its type.
+    write :: N k -> b -> Record r0 -> Record r1
+
+instance Set ('(k,a) ': xs) k a b ('(k,b) ': xs ) where
+    write _ x (_ ::: xs) = x ::: xs
+
+instance Set xs k a b ys => Set ('(k1,a1) ': xs) k a b ('(k1,a1) ': ys) where
+    write l x (y ::: ys) = y ::: write l x ys
+
+-- | Modify a field, without the ability to change its type.
+alter :: Modify r k a a r => N k -> (a -> a) -> Record r -> Record r
+alter = modify
+
+-- | Set a field, without the ability to change its type
+update :: Set r k a a r => N k -> a -> Record r -> Record r
+update = write
 
 class Append xs ys where
     -- | Create a new record by appending two, useful for inheritence / subtyping.
